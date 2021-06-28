e74bdb6eff76d3a6ac7d72ede063ac0535684f9a
Leonid Shapetko <Magicka@yandex.ru>
2021-06-28 19:35:16 +0300

    Завершение всей задачи

--- ExtendVCPractice/ViewController.swift
+++ ExtendVCPractice/ViewController.swift
@@ -9,11 +9,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var coolButton: UIButton!
+    var isAlreadyPressed = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        coolButton.layer.borderWidth = 1.0
+        coolButton.layer.cornerRadius = 2.0
     }
 
-
+    @IBAction func coolButtonPressed(_ sender: UIButton) {
+        if !isAlreadyPressed {
+            showBlackSquare()
+            isAlreadyPressed = true
+            DispatchQueue.main.async {
+                self.coolButton.setTitle("Убрать квадрат", for: .normal)
+            }
+        } else {
+            hideBlackSquare()
+            isAlreadyPressed = false
+            DispatchQueue.main.async {
+                self.coolButton.setTitle("Вернуть квадрат на законное место", for: .normal)
+            }
+        }
+    }
+    
 }
 
+extension ViewController {
+    func showBlackSquare () {
+        let w = UIScreen.main.bounds.width
+        let h = UIScreen.main.bounds.height
+        
+        let blackSquare = UIView(frame: CGRect(x: w/2, y: h/2, width: 25, height: 25))
+        blackSquare.layer.backgroundColor = UIColor.black.cgColor
+        blackSquare.tag = 60
+        self.view.addSubview(blackSquare)
+    }
+    
+    func hideBlackSquare () {
+        if let viewWithCurrentTag = self.view.viewWithTag(60) {
+            viewWithCurrentTag.removeFromSuperview()
+        }
+    }
+}
